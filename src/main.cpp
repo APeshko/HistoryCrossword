509c90f6d2bb6348692810e4fdd0531511af71de
APeshko <m2415803@edu.misis.ru>
2025-05-25 17:47:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    //QFont defaultFont("Arial", 12);
+    //app.setFont(defaultFont)
     MainWindow w;
     w.show();
     return a.exec();
