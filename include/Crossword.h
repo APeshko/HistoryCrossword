65b46fa2a049fbe4747a95844280ffe78d9b32c3
APeshko <m2415803@edu.misis.ru>
2025-06-04 12:14:41 +0300

    Update Crossword.h

--- include/Crossword.h
+++ include/Crossword.h
@@ -1,47 +1,53 @@
 #ifndef CROSSWORD_H
 #define CROSSWORD_H
 
+#include <SFML/Graphics.hpp>
 #include <vector>
 #include <string>
-#include <functional>
+#include <map>
+#include <memory>
+
+struct CrosswordDefinition {
+    std::string word;
+    std::string definition;
+    int startX;
+    int startY;
+    bool horizontal;
+};
 
 class Crossword {
 public:
-    using BackCallback = std::function<void()>;
-
-    Crossword(const std::vector<std::string>& grid, 
-             const std::vector<std::string>& acrossClues,
-             const std::vector<std::string>& downClues);
-
-    const std::vector<std::string>& getGrid() const;
-    const std::vector<std::string>& getAcrossClues() const;
-    const std::vector<std::string>& getDownClues() const;
-    bool isCompleted() const;
-    bool hasSelectedCell() const;
-    std::pair<size_t, size_t> getSelectedCell() const;
-    const std::string& getCurrentInput() const;
-    bool isCellSelected(size_t x, size_t y) const;
-    bool isCellInCurrentWord(size_t x, size_t y) const;
-
-    void selectCell(size_t x, size_t y);
-    void addLetter(char c);
-    void removeLetter();
-    void requestBack();
-    void setBackCallback(BackCallback callback);
+    Crossword(const std::vector<CrosswordDefinition>& definitions, int gridSize);
+    
+    void draw(sf::RenderWindow& window, const sf::Font& font);
+    void handleEvent(const sf::Event& event, sf::RenderWindow& window);
+    bool isComplete() const;
+    void reset();
 
 private:
-    std::vector<std::string> m_grid;
-    std::vector<std::string> m_solution;
-    std::vector<std::string> m_acrossClues;
-    std::vector<std::string> m_downClues;
-    std::pair<size_t, size_t> m_selectedCell;
-    bool m_isHorizontal;
-    std::string m_currentInput;
-    BackCallback m_backCallback;
+    struct Cell {
+        char letter;
+        bool isEditable;
+        bool isHighlighted;
+        std::string userInput;
+        std::vector<int> wordIndices;
+    };
 
-    void updateInput();
-    bool checkSolution() const;
-    std::vector<std::pair<size_t, size_t>> getCurrentWordCells() const;
+    void initializeGrid();
+    void updateHighlight(int x, int y);
+    void checkCompletion();
+    void drawGrid(sf::RenderWindow& window) const;
+    void drawDefinitions(sf::RenderWindow& window, const sf::Font& font) const;
+    void drawLetters(sf::RenderWindow& window, const sf::Font& font) const;
+
+    std::vector<std::vector<Cell>> grid;
+    std::vector<CrosswordDefinition> definitions;
+    std::map<int, bool> completedWords;
+    int gridSize;
+    int cellSize;
+    int selectedX;
+    int selectedY;
+    bool isFullyCompleted;
 };
 
 #endif // CROSSWORD_H
